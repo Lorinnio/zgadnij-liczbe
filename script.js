35cd0da2ac5d1ec091bdce1c0123884edcee55ec
Lorinnio <lorinniofn@gmail.com>
2023-02-17 10:33:06 +0100

    code refactor

--- script.js
+++ script.js
@@ -1,60 +1,70 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const randomNumber = function () {
+  return Math.trunc(Math.random() * 20) + 1;
+};
+const bodyBackground = function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+};
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+const numberWidth = function (width) {
+  document.querySelector('.number').style.width = width;
+};
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+let secretNumber = randomNumber();
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   // kiedy nie ma liczby
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ Brak liczby!';
+    // document.querySelector('.message').textContent = '⛔️ Brak liczby!';
+    displayMessage('⛔️ Brak liczby!');
   }
   // kiedy wygrwa
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Brawo! 🎊';
-    document.querySelector('body').style.backgroundColor = '#40c057';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
+    // document.querySelector('.message').textContent = 'Brawo! 🎊';
+    displayMessage('Brawo! 🎊');
+    bodyBackground('#40c057');
+    numberWidth('30rem');
+    displayNumber(secretNumber);
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
   }
-  // kiedy za wysoko
-  else if (guess > secretNumber) {
+  // kiedy źle
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Za wysoko!';
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? '📈 Za wysoko!' : '📉 Za nisko!';
+      displayMessage(guess > secretNumber ? '📈 Za wysoko!' : '📉 Za nisko!');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent = '💥 Przegrales';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = '#e03131';
-    }
-  }
-  // kiedy za nisko
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Za nisko!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 Przegrales';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = '#e03131';
+      displayMessage('💥 Przegrales');
+      displayScore(0);
+      bodyBackground('#e03131');
     }
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-  document.querySelector('.message').textContent = 'Zacznij zgadywac...';
-  document.querySelector('body').style.backgroundColor = '';
-  document.querySelector('.number').style.width = '';
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.score').textContent = score;
+  secretNumber = randomNumber();
+  displayMessage('Zacznij zgadywac...');
+  bodyBackground('');
+  numberWidth('');
+  displayNumber('?');
+  displayScore(score);
   document.querySelector('.guess').value = '';
 });
