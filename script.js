0e83f09ee9fc022837ac7c20d5c5d6e16f2e6deb
Lorinnio <lorinniofn@gmail.com>
2023-03-27 11:25:29 +0200

    added checking number on enter keypress

--- script.js
+++ script.js
@@ -68,3 +68,41 @@ document.querySelector('.again').addEventListener('click', function () {
   displayScore(score);
   document.querySelector('.guess').value = '';
 });
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Enter') {
+    const guess = Number(document.querySelector('.guess').value);
+    // kiedy nie ma liczby
+    if (!guess) {
+      // document.querySelector('.message').textContent = '⛔️ Brak liczby!';
+      displayMessage('⛔️ Brak liczby!');
+    }
+    // kiedy wygrwa
+    else if (guess === secretNumber) {
+      // document.querySelector('.message').textContent = 'Brawo! 🎊';
+      displayMessage('Brawo! 🎊');
+      bodyBackground('#40c057');
+      numberWidth('30rem');
+      displayNumber(secretNumber);
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+    }
+    // kiedy źle
+    else if (guess !== secretNumber) {
+      if (score > 1) {
+        // document.querySelector('.message').textContent =
+        //   guess > secretNumber ? '📈 Za wysoko!' : '📉 Za nisko!';
+        displayMessage(guess > secretNumber ? '📈 Za wysoko!' : '📉 Za nisko!');
+        score--;
+        displayScore(score);
+      } else {
+        displayMessage('💥 Przegrales');
+        displayScore(0);
+        bodyBackground('#e03131');
+      }
+    }
+  }
+});
